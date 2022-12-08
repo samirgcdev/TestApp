a0f542bb063007dd6f6c85e475e08e98cafa898a
samirgcdev <gcsam222@gmail.com>
2022-12-08 19:27:36 +0545

    Initial Test Maui App

---
+++ MAUIBLeApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MAUIBLeApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MAUIBLeApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUIBLeApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
